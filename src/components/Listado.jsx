64659b059c824eaccdeddc8b13e98f61fc4da604
Haztry <88638634+Haztry@users.noreply.github.com>
2022-05-22 19:20:45 -0700

    Dimensions
    ajustando dimensiones

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -7,7 +7,7 @@ export const Listado = ({pacientes,setPaciente,eliminarPaciente}) => {
    
 
     return (
-        <div className='bg-gradient-to-r from-black to-emerald-400 text-white rounded-md mr-10  w-auto lg:w-3/2 xl:w-auto ml-10 p-3  md:h-screen overflow-y-scroll mt-5 md:mt-0'>
+        <div className='bg-gradient-to-r from-black to-emerald-400 text-white rounded-md mr-10  w-auto lg:w-3/2 xl:w-auto ml-10 p-3  md:h-screen overflow-y-scroll mt-5 sm:mt-0 '>
             <h2 className='text-center text-2xl'>LISTADO DE PACIENTES</h2>
             <p className='text-xl mt-2'>Administra tus <span className='font-bold text-cyan-700'>Paceintes y citas</span></p>
         {pacientes.map(paciente=> 
