215f02952e46ab73b141a7e1a9513362fced7a48
Haztry <al20760554@ite.edu.mx>
2022-05-23 11:13:33 -0700

    Hola
    yeye

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Cabecera = () => {
     return (
-        <h1 className='text-3xl font-bold text-center p-10 m-10 bg-violet-500 rounded-md'>ADMINISTRADOR DE CITAS </h1>
+        <h1 className='text-3xl font-bold text-center text-white p-10 m-10 bg-violet-500 rounded-md'>ADMINISTRADOR DE CITAS </h1>
     )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -109,20 +109,20 @@ export const Formulario = ({setPacientes,pacientes,paciente,setPaciente}) => {
                 {error && <p className='bg-gray-700 text-white p-2  w-full rounded-md text-center font-bold mb-2'>DEBES DE LLENAR TODOS LOS CAMPOS</p>}
                 <div className='p-1'>
                     <label className='font-bold'>Nombre Paciente </label>
-                    <input type="text" id='nombre' className='block w-full rounded-md h-9' placeholder='Nombre completo'value={nombre} onChange={(e) => setNombre(e.target.value)}  />
+                    <input type="text" id='nombre' className='block w-full rounded-md h-9 p-2' placeholder='Nombre completo'value={nombre} onChange={(e) => setNombre(e.target.value)}  />
                 </div>
                 <div className='p-1'>
                     <label className='font-bold'>Fecha cita </label>
-                    <input type="date" id='cita' className='block w-full rounded-md h-9' placeholder='Fecha de cita'value={fecha} onChange={(e) => setFecha(e.target.value)} />
+                    <input type="date" id='cita' className='block w-full rounded-md h-9 p-2' placeholder='Fecha de cita'value={fecha} onChange={(e) => setFecha(e.target.value)} />
                 </div>
                 <div className='p-1'>
                     <label className='font-bold'>Correo </label>
-                    <input type="email" id='correo' className='block rounded-10  w-full rounded-md h-9' placeholder='Correo' value={correo} onChange={(e) => setCorreo(e.target.value)} />
+                    <input type="email" id='correo' className='block rounded-10  w-full rounded-md h-9 p-2' placeholder='Correo' value={correo} onChange={(e) => setCorreo(e.target.value)} />
                 </div>
                 <div className='p-1'>
                     <label className='font-bold'>Sintomas</label>
-                    <textarea className='col-50 row-10 block w-full h-9' id='sintomas' value={sintomas} onChange={(e) => setSintomas(e.target.value)}></textarea>
-                    <input type="submit" id='boton' className='bg-orange-300 text-center text-black mt-5 w-full rounded-md p-1 hover:bg-orange-400 cursor-pointer duration-75' value={paciente.id ? 'Editar paciente' : 'Agregar paciente' }
+                    <textarea className='col-50 row-10 block w-full h-12 p-2' id='sintomas' value={sintomas} onChange={(e) => setSintomas(e.target.value)}></textarea>
+                    <input type="submit" id='boton' className='bg-blue-300 text-center text-black mt-5 w-full rounded-md p-1 hover:bg-blue-400 cursor-pointer duration-75' value={paciente.id ? 'Editar paciente' : 'Agregar paciente' }
                     
                     />
                 </div>
