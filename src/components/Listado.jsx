b689e19136e9a0d9b21398f8881704e59048034e
Haztry <88638634+Haztry@users.noreply.github.com>
2022-05-22 23:33:35 -0700

    y
    y

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -8,7 +8,7 @@ export const Listado = ({pacientes,setPaciente,eliminarPaciente}) => {
 
     return (
         <div className='md:max-h-[390px] bg-violet-400 text-white rounded-md mr-10  w-auto lg:w-3/2 xl:w-auto ml-10 p-3  md:h-screen overflow-y-scroll mt-5 sm:mt-0 '>
-            <h2 className='text-center text-2xl'>LISTADO DE PACIENTES</h2>
+            <h2 className='text-center text-2xl font-bold'>LISTADO DE PACIENTES</h2>
             <p className='text-xl mt-2'>Administra tus <span className='font-bold text-cyan-700'>Paceintes y citas</span></p>
         {pacientes.map(paciente=> 
         <Paciente 
